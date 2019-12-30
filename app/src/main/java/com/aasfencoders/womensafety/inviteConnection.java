6eec7a480ef78e77d36a43f56860f5c121030a77
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-12-30 16:06:30 +0530

    Invite Connecions layout done

---
+++ app/src/main/java/com/aasfencoders/womensafety/inviteConnection.java
@@ -0,0 +1,14 @@
+package com.aasfencoders.womensafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inviteConnection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_connection);
+    }
+}
--- app/src/main/java/com/aasfencoders/womensafety/ui_fragment/HomeFragment.java
+++ app/src/main/java/com/aasfencoders/womensafety/ui_fragment/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +14,33 @@ import com.aasfencoders.womensafety.R;
 
 public class HomeFragment extends Fragment {
 
+    private Button inviteNewConnection;
+    private Button showConnection;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        inviteNewConnection = view.findViewById(R.id.inviteConnection);
+        showConnection = view.findViewById(R.id.showConnection);
+
+        // action triggered when the Invite New Connection button is tpressed
+        inviteNewConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        // action triggered when the show connection button is pressed
+        showConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
     }
 }
