ec99e5ebba808183e6de9b2348fc18d5ad66b5e2
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-12-30 19:13:19 +0530

    Contacts List name fetch displayed as text

--- app/src/main/java/com/aasfencoders/womensafety/ContactActivity.java
+++ app/src/main/java/com/aasfencoders/womensafety/ContactActivity.java
@@ -1,14 +1,55 @@
 package com.aasfencoders.womensafety;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
+
+
+    String tv;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        textView = findViewById(R.id.sample_text);
+
+        getContactList();
     }
+
+    private void getContactList() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            tv = null;
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+
+                tv += id + " " + name + "\n\n";
+            }
+            textView.setText(tv);
+        }
+        if (cur != null) {
+            cur.close();
+        }
+    }
+
 }
