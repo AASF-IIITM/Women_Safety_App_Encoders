9b61135ac5c16d3d0c6a8816a73977fca9ddc09d
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-01-04 22:09:27 +0530

    Form set and Profile name sored to database

--- app/src/main/java/com/aasfencoders/womensafety/MainActivity.java
+++ app/src/main/java/com/aasfencoders/womensafety/MainActivity.java
@@ -3,20 +3,27 @@ package com.aasfencoders.womensafety;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
-import android.util.Log;
+import android.text.Html;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aasfencoders.womensafety.utilities.CheckNetworkConnection;
-import com.aasfencoders.womensafety.utilities.NetworkDialog;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.util.data.PhoneNumberUtils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.yarolegovich.lovelydialog.LovelyTextInputDialog;
 
 import java.util.Arrays;
 
@@ -31,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     SharedPreferences sharedPreferences;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mFirebaseReference;
+
+    ProgressBar progressBar;
+    TextView textView;
 
     private void onSignedInInitialize(String username) {
         Intent intent = new Intent(this,BottomNavigationActivity.class);
@@ -59,24 +72,45 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public boolean showNetworkDialog(Context mContext) {
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(mContext);
+        builder.setCancelable(false);
+        builder.setTitle(Html.fromHtml("<h6><font color='#465ba6'>You are offline!</font></h6>"));
+        builder.setMessage( Html.fromHtml("Connect your phone to internet/WiFi connection and then press on RETRY button."));
+        builder.setIcon(R.drawable.ic_warning_pink_24dp);
+        builder.setNegativeButton(Html.fromHtml("<h7><font color='#465ba6'>RETRY</font></h7>"), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+                checkConnection();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progress);
+        textView  = findViewById(R.id.loadingUser);
+
         ANONYMOUS = getString(R.string.ANONYMOUS);
         sharedPreferences = MainActivity.this.getSharedPreferences(getString(R.string.package_name), Context.MODE_PRIVATE);
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseReference = mFirebaseDatabase.getReference();
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @SuppressLint("RestrictedApi")
             @Override
             public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    TelephonyManager tm = (TelephonyManager)MainActivity.this.getSystemService(MainActivity.this.TELEPHONY_SERVICE);
-                    String countryCodeValue = tm.getNetworkCountryIso();
-                    mUserPhoneNumber = "+" + PhoneNumberUtils.getCountryCode(countryCodeValue) + user.getPhoneNumber();
-                    onSignedInInitialize(mUserPhoneNumber);
-
+                    mUserPhoneNumber = user.getPhoneNumber();
                     int firstTime = sharedPreferences.getInt(getString(R.string.firstform), 0);
                     if (firstTime == 0) {
                         checkConnection();
@@ -118,15 +152,34 @@ public class MainActivity extends AppCompatActivity {
     private void checkConnection(){
         boolean state = CheckNetworkConnection.checkNetwork(MainActivity.this);
         if (state) {
+            progressBar.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.INVISIBLE);
             showForm();
         } else {
-            if(NetworkDialog.showNetworkDialog(MainActivity.this)){
-                checkConnection();
-            }
+            showNetworkDialog(MainActivity.this);
         }
     }
 
     private void showForm(){
-        
+        new LovelyTextInputDialog(this, R.style.TintTheme)
+                .setTopColorRes(R.color.dialogColour)
+                .setTitle(R.string.text_input_title)
+                .setMessage(R.string.text_input_message)
+                .setIcon(R.drawable.ic_person_outline_white_36dp)
+                .setInputFilter(R.string.text_input_error_message, new LovelyTextInputDialog.TextFilter() {
+                    @Override
+                    public boolean check(String text) {
+                        return !text.isEmpty();
+                    }
+                })
+                .setConfirmButton(R.string.upload, new LovelyTextInputDialog.OnTextInputConfirmListener() {
+                    @Override
+                    public void onTextInputConfirmed(String text) {
+                        mFirebaseReference.child(getString(R.string.users)).child(mUserPhoneNumber).child(getString(R.string.profile)).child(getString(R.string.name)).setValue(text.trim());
+                        Toast.makeText(MainActivity.this, "Data Stored in Server", Toast.LENGTH_SHORT).show();
+                        onSignedInInitialize(mUserPhoneNumber);
+                    }
+                })
+                .show();
     }
 }
