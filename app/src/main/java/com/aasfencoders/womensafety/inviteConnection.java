c702242bebee838cb202d02cd4c0e31f588591eb
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-01-08 18:07:21 +0530

    Permission to fetch Read Contacts done

--- app/src/main/java/com/aasfencoders/womensafety/inviteConnection.java
+++ app/src/main/java/com/aasfencoders/womensafety/inviteConnection.java
@@ -1,10 +1,15 @@
 package com.aasfencoders.womensafety;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,6 +39,38 @@ public class inviteConnection extends AppCompatActivity {
 
     private ArrayList<InviteSentClass> inviteList;
 
+    private void contact(){
+        Intent intent = new Intent(inviteConnection.this , ContactActivity.class);
+        startActivity(intent);
+    }
+
+    private void checkContactPermission(){
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED  ) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+        }else{
+            contact();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                    contact();
+                } else {
+                    Toast.makeText(this , getString(R.string.permissionDenied) , Toast.LENGTH_LONG).show();
+                }
+            }  else {
+                Toast.makeText(this , getString(R.string.permissionDenied) , Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,8 +88,7 @@ public class inviteConnection extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(inviteConnection.this, ContactActivity.class);
-                startActivity(intent);
+                checkContactPermission();
             }
         });
 
