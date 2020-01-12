40675a105fb3ad41d99efa9e5de3d4c7f37ef68e
Mehul Jain <mehuljain08032001@gmail.com>
2020-01-12 14:12:54 +0530

    MapsActivity added

--- app/src/main/java/com/aasfencoders/womensafety/ui_fragment/TrackMeFragment.java
+++ app/src/main/java/com/aasfencoders/womensafety/ui_fragment/TrackMeFragment.java
@@ -1,14 +1,17 @@
 package com.aasfencoders.womensafety.ui_fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.aasfencoders.womensafety.MapsActivity;
 import com.aasfencoders.womensafety.R;
 
 public class TrackMeFragment extends Fragment {
@@ -16,6 +19,16 @@ public class TrackMeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_trackme,container,false);
+        View view =  inflater.inflate(R.layout.fragment_trackme,container,false);
+        Button click= view.findViewById(R.id.click);
+        click.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        return  view;
     }
 }
