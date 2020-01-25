1a918297be0a38e256ef94fa4573e30ca9cb7477
Mehul Jain <mehuljain08032001@gmail.com>
2020-01-25 23:03:26 +0530

    invitation layout redefined

--- app/src/main/java/com/aasfencoders/womensafety/adapter/InviteAdapter.java
+++ app/src/main/java/com/aasfencoders/womensafety/adapter/InviteAdapter.java
@@ -3,11 +3,13 @@ package com.aasfencoders.womensafety.adapter;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +21,7 @@ import com.aasfencoders.womensafety.data.DataContract;
 import com.aasfencoders.womensafety.inviteConnection;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class InviteAdapter extends CursorAdapter {
     private Context mContext;
@@ -49,9 +52,28 @@ public class InviteAdapter extends CursorAdapter {
         String number = cursor.getString(numberColIndex);
         String status = cursor.getString(invitationColIndex);
 
+
+        ImageView image = view.findViewById(R.id.invite_view_image);
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(230), rnd.nextInt(230), rnd.nextInt(230));
+        image.setBackgroundColor(color);
+
         nameView.setText(name);
         numberView.setText(number);
-        statusView.setText(status);
+        statusView.setText(status.toUpperCase());
+
+        statusView.setTextColor(Color.rgb(255, 255, 255));
+        switch (status) {
+            case "invited":
+                statusView.setBackgroundColor(Color.rgb(252,132,3));
+                break;
+            case "rejected":
+                statusView.setBackgroundColor(Color.rgb(254,42,3));
+                break;
+            case "matched":
+                statusView.setBackgroundColor(Color.rgb(45,188,39));
+                break;
+        }
 
     }
 }
